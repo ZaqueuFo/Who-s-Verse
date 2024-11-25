12bf42ec2df37e5f3c492d4d84ad98fe50c90e73
ZaqueuFo <zaqueu.duraesfo@sptech.school>
2024-11-25 04:46:31 -0300

    Arruma Bug

---
+++ src/models/usuarioModel.js
@@ -0,0 +1,52 @@
+var database = require("../database/config")
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT idUsuario, nome, email, cpf FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(nome, email, senha, cpf) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cpf);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO usuario (cpf, nome, email, senha) VALUES ('${cpf}', '${nome}', '${email}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function doutor(doutor, idUsuario) {
+    var instrucaoSql = `
+        UPDATE usuario set fkDoutor = ${doutor} WHERE idUsuario = ${idUsuario};
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function listarRanking() {
+    var instrucaoSql = `
+        SELECT d.nome AS doutor, 
+       COUNT(u.fkDoutor) AS votos
+       FROM doutores d
+       LEFT JOIN usuario u ON u.fkDoutor = d.idDoutores
+       GROUP BY d.nome
+       ORDER BY votos DESC;  
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    autenticar,
+    cadastrar,
+    doutor,
+    listarRanking // Adicione isso aqui
+};
+
